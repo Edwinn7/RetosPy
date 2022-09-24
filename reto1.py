382b8bca7449637838b669abf911ace529c6e8cb
209 <209@academicos.cesde.edu.co>
2022-09-24 13:14:23 -0500

    prueba resulta

--- reto1.py
+++ reto1.py
@@ -1,6 +1,8 @@
 # crear funcion jennifer
 
-
 #Recorre arreglo edwin
 
 #promediar arreglo alexander
+elemento = len(arreglo)
+promedio = numero / 20
+print(f"El promedio es {promedio}")
--- reto2.py
+++ reto2.py
@@ -1,7 +1,17 @@
 # recibir ancho y alto por teclado alexander
-
+altura = int(input("digite altura"))
+base = int(input("digite base"))
 # calcular el area con alt,anch edwin   
 
 #calcular el perimetro con alt anch jennifer
 
 #graficar alexander
+# altura = 0
+# base = 0
+# width = 10
+# height = 5
+     
+# graph.rect(x, y, width, height) 
+      
+
+print(altura*'*',base*'*')
