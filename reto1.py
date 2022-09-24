c28991f43cf4f55270f040c93cef3793b9f205eb
David Osorio <daviosood@gmail.com>
2022-09-24 13:04:44 -0500

    puntos resueltos

--- reto1.py
+++ reto1.py
@@ -1,6 +1,10 @@
 # crear funcion jennifer
 
 
-#Recorre arreglo edwin
-
+#Recorre arreglo y sumar edwin
+sumar=0
+for numero in nuemeros:
+    sumar=sum(numeros)
+    print(f'La suma de la lista es {sumar}')
+    
 #promediar arreglo alexander
